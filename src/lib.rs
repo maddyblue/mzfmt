4c1162444663639df8fb21a38fe591d3d3eebaa7
Matt Jibson <matt.jibson@gmail.com>
2021-09-26 22:34:13 -0600

    add SelectItem

--- src/lib.rs
+++ src/lib.rs
@@ -39,13 +39,17 @@ fn doc_display<'a, T: Display>(v: &T) -> RcDoc<'a, ()> {
     RcDoc::text(format!("{}", v))
 }
 
+fn nest<'a>(title: RcDoc<'a>, v: RcDoc<'a>) -> RcDoc<'a> {
+    RcDoc::intersperse(vec![title, v], Doc::line())
+        .nest(TAB)
+        .group()
+}
+
 fn nest_title<S>(title: S, v: RcDoc) -> RcDoc
 where
     S: Into<String>,
 {
-    RcDoc::intersperse(vec![RcDoc::text(title.into()), v], Doc::line())
-        .nest(TAB)
-        .group()
+    nest(RcDoc::text(title.into()), v)
 }
 
 fn title_comma_separate<'a, F, T, S>(title: S, f: F, v: &'a [T]) -> RcDoc<'a, ()>
@@ -57,9 +61,7 @@ where
     if v.is_empty() {
         title
     } else {
-        RcDoc::intersperse(vec![title, comma_separate(f, v)], Doc::line())
-            .group()
-            .nest(TAB)
+        nest(title, comma_separate(f, v))
     }
 }
 
@@ -314,7 +316,7 @@ fn doc_select(v: &Select<Raw>) -> RcDoc {
                 "".into()
             }
         ),
-        doc_display,
+        doc_select_item,
         &v.projection,
     ));
     if !v.from.is_empty() {
@@ -337,6 +339,22 @@ fn doc_select(v: &Select<Raw>) -> RcDoc {
         ));
     }
     RcDoc::intersperse(docs, Doc::line()).group()
+}
+
+fn doc_select_item(v: &SelectItem<Raw>) -> RcDoc {
+    match v {
+        SelectItem::Expr { expr, alias } => {
+            let mut doc = doc_expr(expr);
+            if let Some(alias) = alias {
+                doc = nest(
+                    doc,
+                    RcDoc::concat(vec![RcDoc::text("AS "), doc_display(alias)]),
+                );
+            }
+            doc
+        }
+        _ => doc_display(v),
+    }
 }
 
 fn doc_expr(v: &Expr<Raw>) -> RcDoc {
@@ -385,11 +403,9 @@ mod tests {
             //"CREATE VIEW view_1 (col_2) AS SELECT * FROM (VALUES (CAST(0.9841192240680561 AS float)), (CAST(0.37823189648731315 AS float)), (CAST(0.8390174385199045 AS float)), (CAST(0.22188376517105302 AS float)), (CAST(0.5787854533815643 AS float)), (CAST(0.7234205380688273 AS float)), (CAST(0.39567191795118384 AS float)), (CAST(0.4348712893998896 AS float)), (CAST(0.8856762904388714 AS float)), (CAST(0.7704453261942663 AS float)), (CAST(0.5133022896871524 AS float)), (CAST(NULL AS float)), (CAST(0.5170637540787644 AS float)), (CAST(0.6762831752745486 AS float)), (CAST(0.2424964369655378 AS float)), (CAST(0.031422253928415134 AS float)), (CAST(0.7791437964022883 AS float)), (CAST(0.7976069716256476 AS float)), (CAST(0.49670516047468893 AS float))) AS tab_2",
             //"SELECT CAST(CAST(26884955 AS int) AS int) INTERSECT SELECT view_537.col_1111 FROM view_537 WHERE NOT ((view_537.col_1111 > CAST(- 406118011 AS int)) <= ((CAST(182393886938628546 AS bigint) % CAST(- 7505760285872234247 AS bigint)) >= CAST(6268575506156459773 AS bigint)));",
             //"SELECT tab_111.col_1 FROM tab_1 AS tab_111 FULL JOIN tab_1 AS tab_112 ON (CAST(- 2474187877618617778 AS bigint) = CAST(2715953958593069068 AS bigint)) RIGHT JOIN tab_1 AS tab_113 ON (tab_113.col_1 > CAST('' AS text));",
-            "select limit 1",
-            "SELECT
-    *
-FROM
-    [u123 AS materialize.public.foo];",
+            //"select limit 1",
+            //"SELECT * FROM [u123 AS materialize.public.foo];",
+            "select 1+2 as eeeee, *",
         ];
 
         for stmt in stmts {
@@ -404,7 +420,7 @@ FROM
                     last = s;
                 }
                 n += 1;
-                if n > stmt.len() {
+                if n > (stmt.len() + 5) {
                     break;
                 }
             }
