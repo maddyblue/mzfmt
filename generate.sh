5243bbfe744935d0246573e0ecbcf43f4191677d
Matt Jibson <matt.jibson@gmail.com>
2022-12-27 15:24:48 -0700

    snapshot

--- generate.sh
+++ generate.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Need to:
+# - comment out workspace-hack deps in the ore Cargo.toml
+# - comment out workspace-hack deps in the sql-parser Cargo.toml
+# - to ore's cast.rs, target32 mod, add: cast_from!(u64, usize);
+
 RUSTFLAGS='' wasm-pack build --target web --out-dir ../docs web
 
 # python3 -m http.server
